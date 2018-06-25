19da4b916ea0f388f5949b2205648c4ec2fb823d
Mark Bastian <markbastian@gmail.com>
2018-06-24 23:29:40 -0600

    superhero example

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-RC2"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [datascript "0.16.3"]
                  [clojure-csv/clojure-csv "2.0.1"]
                  [clj-time "0.14.2"]
