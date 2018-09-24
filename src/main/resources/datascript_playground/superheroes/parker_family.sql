29968ed36dca39a7fcdf2e989e90405610d4ae0f
Mark Bastian <markbastian@gmail.com>
2018-09-24 15:04:15 -0600

    Added query.

--- src/main/resources/datascript_playground/superheroes/parker_family.sql
+++ src/main/resources/datascript_playground/superheroes/parker_family.sql
@@ -31,3 +31,26 @@ INSERT INTO "PUBLIC"."CHILDREN" ("ID", "PARENTID", "CHILDID") VALUES (1, 2, 1);
 INSERT INTO "PUBLIC"."CHILDREN" ("ID", "PARENTID", "CHILDID") VALUES (2, 3, 1);
 INSERT INTO "PUBLIC"."CHILDREN" ("ID", "PARENTID", "CHILDID") VALUES (3, 6, 2);
 INSERT INTO "PUBLIC"."CHILDREN" ("ID", "PARENTID", "CHILDID") VALUES (4, 6, 4);
+
+-- Example query
+SELECT a.alias, p.name, parent, grandparent, aunt_uncle, aunt_uncle_spouse
+from aliases as a
+join people  as p on a.personID = p.id
+join (
+       select (select name from people as n where n.id = c1.childid ) as child
+              , (select name from people as n where n.id = c1.parentid) as parent
+              , (select name from people as n where n.id = c2.parentid) as grandparent
+              , (select name from people as n where n.id = c3.childid ) as aunt_uncle
+              , (select name from people as n where n.id = m.spouseid) as aunt_uncle_spouse
+              , c1.childid  as childid
+               , c1.parentid as parentid
+              , c2.parentid as grandparentid
+              , c3.childid  as aunt_uncleid
+              , m.spouseid  as aunt_uncle_spouseid
+       from children as c1 -- parents
+       join children as c2 on c2.childID = c1.parentID -- grandparents
+       join children as c3 on c3.parentID = c2.parentID -- siblings of parents
+                              and c2.childID != c3.childID -- not parents
+       left join marriages as m on c3.childid = m.personID -- spouses of siblings of parents
+) as aunt_uncle on aunt_uncle.childid = p.id
+where a.alias = 'Spidey'
